3a028f9fd52ffa78fca7ef3dfa2f890d5d96857b
👋 Hi, I'm Aamna <asyed021@fiu.edu>
2026-08-04 03:06:42 -0400

    Add files via upload
    Add maze puzzle project

---
+++ README.md
@@ -0,0 +1,32 @@
+\# Maze Puzzle Search Algorithms
+
+
+
+This project implements maze-solving algorithms in Python.
+
+
+
+\## Algorithms
+
+\- Breadth-First Search (BFS)
+
+\- Depth-First Search (DFS)
+
+\- A\* Search
+
+
+
+\## Requirements
+
+
+
+pip install -r requirements.txt
+
+
+
+\## Run
+
+
+
+python main.py
+
---
+++ submitted.py
@@ -0,0 +1,34 @@
+# submitted.py
+# ---------------
+# Licensing Information:
+# This HW is inspired by previous work by University of Illinois at Urbana-Champaign
+
+
+"""
+This is the main entry point for MP5. You should only modify code
+within this file -- the unrevised staff files will be used for all other
+files and classes when code is run, so be careful to not modify anything else.
+"""
+# submitted should return the path.
+# The path should be a list of tuples in the form (row, col) that correspond
+# to the positions of the path taken by your search algorithm.
+# maze is a Maze object based on the maze from the file specified by input filename
+# searchMethod is the search method specified by --method flag (bfs,dfs,astar,astar_multi)
+
+def bfs(maze):
+
+    #TODO: Implement bfs function
+
+    return []
+
+def astar_single(maze):
+
+    #TODO: Implement astar_single
+
+    return []
+
+
+def dfs(maze):
+    # TODO: Implement dfs
+
+    return []
